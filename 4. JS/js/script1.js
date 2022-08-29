7e96285f2bec22d38fbfa62ef407725d79ab18f4
ArmanTapayev <arman.tapayev@gmail.com>
2022-08-29 13:20:39 +0600

    First Commit

---
+++ 4. JS/js/script1.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  alert("Working!");
+}
+
+function buttonDate() {
+  alert(Date());
+}
+function pChange() {
+  document.getElementById("changeText").innerHTML = "new Text!!!";
+}
